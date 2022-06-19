e64e1d77eabe9e9dcdee97bdd33fd4cf8dc98273
Romaric Philogène <rphilogene@qovery.com>
2022-06-19 15:44:30 +0200

    chore: add first example

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# terraform-examples
-This repository contains ready to use Terraform examples
+# Qovery Terraform Examples
+
+This repository contains ready to use Terraform [examples](/examples). Feel free to use them, modify them and share them with us
+and [our community](https://discuss.qovery.com).
+
+> Watch our live session on [How Getting Started with Terraform and Qovery on AWS](https://www.youtube.com/watch?v=l8D6IrEoIgw)
+
+## Usage
+
+1. [Install Terraform](https://learn.hashicorp.com/tutorials/terraform/install-cli)
+2. Clone this repository
+3. Go to the [examples](examples) folder
+4. Pick the example you want and follow the instruction.
+
+## Documentation
+
+* [Qovery Terraform provider](https://registry.terraform.io/providers/Qovery/qovery/latest)
+* [Qovery Documentation](https://hub.qovery.com)
+
+## Examples
+
+Here is a list of all the examples
+
+| Description                                                               | Folder                                                |
+|---------------------------------------------------------------------------|-------------------------------------------------------|
+| Deploy an application from GitHub on AWS and make it accessible via HTTPS | [here](/examples/deploy-github-app-on-aws-with-https) |
+
+## How to contribute
+
+* Do you need any help? Open an issue.
+* Do you want to share a template? Open a Pull Request.
+
+## Contact
+
+Feel free to contact us via [our public forum](https://discuss.qovery.com)
---
+++ examples/deploy-github-app-on-aws-with-https/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    qovery = {
+      source = "qovery/qovery"
+    }
+  }
+}
+
+provider "qovery" {
+  token = var.qovery_access_token
+}
+
+resource "qovery_aws_credentials" "my_aws_creds" {
+  organization_id   = var.qovery_organization_id
+  name              = "URL Shortener"
+  access_key_id     = var.aws_access_key_id
+  secret_access_key = var.aws_secret_access_key
+}
+
+resource "qovery_cluster" "my_cluster" {
+  organization_id   = var.qovery_organization_id
+  credentials_id    = qovery_aws_credentials.my_aws_creds.id
+  name              = "Demo cluster"
+  description       = "Terraform demo cluster"
+  cloud_provider    = "AWS"
+  region            = "us-east-2"
+  instance_type     = "T3A_MEDIUM"
+  min_running_nodes = 3
+  max_running_nodes = 4
+  state             = "RUNNING"
+
+  depends_on = [
+    qovery_aws_credentials.my_aws_creds
+  ]
+}
+
+resource "qovery_project" "my_project" {
+  organization_id = var.qovery_organization_id
+  name            = "URL Shortener"
+
+  depends_on = [
+    qovery_cluster.my_cluster
+  ]
+}
+
+resource "qovery_environment" "production" {
+  project_id = qovery_project.my_project.id
+  name       = "dev"
+
+  depends_on = [
+    qovery_project.my_project
+  ]
+}
+
+resource "qovery_application" "backend" {
+  environment_id = qovery_environment.production.id
+  name           = "backend"
+  cpu            = 500
+  memory         = 256
+  state          = "RUNNING"
+  git_repository = {
+    url       = "https://github.com/evoxmusic/ShortMe-URL-Shortener.git"
+    branch    = "main"
+    root_path = "/"
+  }
+  build_mode            = "BUILDPACKS"
+  buildpack_language    = "PYTHON"
+  min_running_instances = 1
+  max_running_instances = 1
+  ports                 = [
+    {
+      internal_port       = 3333
+      external_port       = 443
+      protocol            = "HTTP"
+      publicly_accessible = true
+    }
+  ]
+  environment_variables = [
+    {
+      key   = "PORT"
+      value = "3333"
+    },
+    {
+      key   = "DEBUG"
+      value = "false"
+    }
+  ]
+
+  depends_on = [
+    qovery_environment.production,
+  ]
+}
